cf395a74bd4763210c61f4de7c6e0c22dd60606d
Neil <neil.snowdog@gmail.com>
2026-05-03 15:41:25 +0100

    tf cloud files

---
+++ 18-terraform-cloud/provider.tf
@@ -0,0 +1,26 @@
+terraform {
+
+  cloud {
+    organization = "Slowbius-2468"
+
+    workspaces {
+      name = "terraform-cli"
+    }
+  }
+
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
